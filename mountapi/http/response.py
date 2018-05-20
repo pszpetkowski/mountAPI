c6b65737498d5c8eb6cec250eab4078ff0f39e92
Piotr <piotr.szpetkowski@pyquest.space>
2018-05-20 23:56:43 +0200

    Add from_http_client_error to Reponse class
    Rename status_code to status in Reponse class

--- mountapi/http/response.py
+++ mountapi/http/response.py
@@ -1,17 +1,18 @@
 import time
 from wsgiref.handlers import format_date_time
 
-from mountapi.http import status
+from mountapi.http.exceptions import HttpClientError
+from mountapi.http.status import OK_200
 from mountapi.lib import json
 
 
 class Response:
-    def __init__(self, content, status_code=None):
+    def __init__(self, content, status=None):
         if isinstance(content, str):
             self._content = content.encode()
         else:
             self._content = json.dumps(content).encode()
-        self._status_code = status_code or status.OK_200
+        self._status = status or OK_200
 
     @classmethod
     def from_result(cls, result):
@@ -20,14 +21,18 @@ class Response:
         else:
             return Response(result)
 
-    def to_bytes(self):
+    @classmethod
+    def from_http_client_error(cls, e: HttpClientError):
+        return Response(e.message, status=e.status)
+
+    def to_bytes(self) -> bytes:
         return (
             b'HTTP/1.1 %i %b\r\n'
             b'Date: %b\r\n'
             b'Connection: closed\r\n'
             b'Content-Type: application/json\r\n\r\n'
             b'%b' % (
-                self._status_code['code'], self._status_code['reason'],
+                self._status['code'], self._status['reason'],
                 format_date_time(time.time()).encode(),
                 self._content,
             )
