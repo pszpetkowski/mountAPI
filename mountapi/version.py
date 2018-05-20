2f90be3374f63f5bdf73ff6f7c74754cac1153d7
Piotr <piotr.szpetkowski@pyquest.space>
2018-05-20 11:37:12 +0200

    Add various version strings

---
+++ mountapi/version.py
@@ -0,0 +1,7 @@
+__name__ = 'mountapi'
+__url__ = 'https://mountapi.org'
+__version__ = 'unreleased'
+__author__ = 'Piotr Szpetkowski'
+__author_email__ = 'piotr.szpetkowski@pyquest.space'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Piotr Szpetkowski'
